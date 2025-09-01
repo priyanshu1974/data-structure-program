a7cda359dfdd6bce1476039576bbd126a9f56da2
priyanshu1974 <priyanshu.kushwahcse2024@indoreinstitute.com>
2025-09-01 11:20:23 +0530

    Add files via upload

---
+++ sum of square of natural.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int i,n,sum=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        sum+=i*i;
+    }
+    printf("sum of square of first%d natural number is:%d\n",n,sum);
+
+    return 0;
+}
